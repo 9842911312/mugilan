b8cf23953d870d76c812a93eba2ea691f867d808
9842911312 <36265861+9842911312@users.noreply.github.com>
2018-02-14 20:10:34 -0800

    oddoreven.py
    the given number is odd or even

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+n=int(input("enter the given number"))
+if(n>=100000):
+   print("invalid number")
+elif(n%2==0):
+   print("given number is even")
+else:
+   print("given number is odd")
+    
+    

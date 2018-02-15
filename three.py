64ec3fd3321fe080d3ba067596f12e37a4340361
9842911312 <36265861+9842911312@users.noreply.github.com>
2018-02-14 21:07:46 -0800

    three.py
    the given the number is three addd

---
+++ three.py
@@ -0,0 +1,9 @@
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+z=int(input("enter the number"))
+if(x>y):
+    print("x is greathan")
+elif(y>z):
+    print("y is greathan")
+else:
+``  print("z is greathan")
